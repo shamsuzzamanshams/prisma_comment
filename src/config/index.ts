94c074534001c83aa9c985ade8c9c318d5bb2e4c
shamsuzzamanshams <shams30200@gmail.com>
2026-07-06 23:11:09 +0600

    add payment method

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,5 +14,6 @@ export default {
 	jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
 	stripe_product_id: process.env.STRIPE_PRODUCT_ID!,
 	stripe_secret_key: process.env.STRIPE_SECRET_KEY!,
+	stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET!,
 
 }
